3b57b7118699e0f649a322d38fcec48d6bb9cc80
Noah012 <noahpierce185016@gmail.com>
2019-02-12 20:52:31 -0600

    added JavaDocs for the readFile method, and comments to the method

--- HammingDist.java
+++ HammingDist.java
@@ -20,20 +20,34 @@ public class HammingDist
 		return 0;
 	}
 	
+	/**
+	 * This loads in the Mesonet.txt file and reads it through the BufferedReader object.
+	 * After reading through the first 3 lines of the file =, it stores the fourth line in a string array to be able to 
+	 * extract the stations codes from the file
+	 * 
+	 * @param filename
+	 * @throws IOException
+	 */
 	public void readFile (String filename) throws IOException
 	{
+		//declars a string variable to store the stations code from the string array to be added to the arraylist
 		String sCode = "";
-		@SuppressWarnings("resource")
+		
+		//Declares buffered reader object to read the Mesonet.txt file
 		BufferedReader br = new BufferedReader(new FileReader(filename));
 		
+		//reads through the first 3 lines of the .txt file to skip the first 3 lines for it is uneeded info
 		br.readLine();
 		br.readLine();
 		br.readLine();
 		
-		
+		//stores the 4th line of the Mesonet.txt file into a string
 		filename = br.readLine();
+		
+		// splits the line read into the variable and stores it into the a String array
 		String[] strg = filename.split(" ");
 		
+		//this loops through the Mesonet.txt file and extracts the stations codes into the stationsCodes ArrayList
 		while(!(filename == null))
 		{
 			sCode = strg[1];
