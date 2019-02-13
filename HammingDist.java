21d4d5dbbb3eeb5dec3c101b2cca341c18e5837b
Noah012 <noahpierce185016@gmail.com>
2019-02-12 20:38:09 -0600

    created abd finished the readFile method

--- HammingDist.java
+++ HammingDist.java
@@ -1,11 +1,52 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class HammingDist
 {
-	public HammingDist(String station1, String station2)
+	private ArrayList<String> stationsCodes = new ArrayList<String>();
+	private int totalStations = 120;
+	
+	public HammingDist(String station1, String station2) throws IOException
 	{
+		readFile("Mesonet.txt");
+	}
+	
+	public int calculatingDstance(String x, String y)
+	{
+		
+		return 0;
+	}
+	
+	public void readFile (String filename) throws IOException
+	{
+		String sCode = "";
+		@SuppressWarnings("resource")
+		BufferedReader br = new BufferedReader(new FileReader(filename));
+		
+		br.readLine();
+		br.readLine();
+		br.readLine();
+		
+		
+		filename = br.readLine();
+		String[] strg = filename.split(" ");
+		
+		while(!(filename == null))
+		{
+			sCode = strg[1];
+			
+			stationsCodes.add(sCode);
+			filename = br.readLine();
+			System.out.println(filename);
+			if (filename != null)
+			{
+				strg = filename.split(" ");
+			}
+		}
 		
 	}
 	
-
 }
